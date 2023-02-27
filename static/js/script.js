dc4f3023389cd65bc108b4f0bd61f3d45dc5cb82
MajaG <99552728+MajaLu@users.noreply.github.com>
2023-02-27 19:18:57 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -23,7 +23,7 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://api.unsplash.com/search/photos?query=#{name}&content_filter=high&client_id=#BWWvgGxoJ0mfCadHhcXU8j3ll6LdhReuTqC0RsQdixc')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
     
     search: function () {
